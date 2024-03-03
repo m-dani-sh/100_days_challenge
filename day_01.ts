936e94ae1d7ac7baf6e8eeddb552b1d8661efcb4
m-dani-sh <160328669+m-dani-sh@users.noreply.github.com>
2024-03-03 09:41:19 -0800

    Update day_01.ts

--- day_01.ts
+++ day_01.ts
@@ -10,6 +10,6 @@ console.log(personName.toUpperCase());
 let words = personName.split(" ");
 let titlecaseName = "";
  for (let i = 0; i < words.length; i++) {
-titlecaseName += words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase() + " ";} ; 
+titlecaseName += words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase() + " "}; 
 console.log(titlecaseName);
 
