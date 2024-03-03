c98750d039c01917bb6c92a0ba8567fd1d32fc11
m-dani-sh <160328669+m-dani-sh@users.noreply.github.com>
2024-03-03 09:40:45 -0800

    Update day_01.js

--- day_01.js
+++ day_01.js
@@ -8,7 +8,5 @@ console.log(personName.toUpperCase());
 var words = personName.split(" ");
 var titlecaseName = "";
 for (var i = 0; i < words.length; i++) {
-    titlecaseName += words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase() + " ";
-}
-;
+    titlecaseName += words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase() + " "};
 console.log(titlecaseName);
