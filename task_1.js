14105dad334846917d1e85b3d2cbde964d92b1fc
Muhammad Danish <160328669+m-dani-sh@users.noreply.github.com>
2024-03-03 23:28:42 -0800

    Delete task_1.js

--- task_1.js
+++
@@ -1,5 +0,0 @@
-//Question #1;
-console.log("This is Addition:".concat(5 + 3));
-console.log("This is Subtracion:".concat(10 - 2));
-console.log("This is Multiplication:".concat(4 * 2));
-console.log("This is division:".concat(16 / 2));
