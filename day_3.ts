4919cbb8a0cb05999bf33d164d8a9787dc12e7df
Muhammad Danish <160328669+m-dani-sh@users.noreply.github.com>
2024-03-03 23:33:19 -0800

    day_3.ts

---
+++ day_3.ts
@@ -0,0 +1,18 @@
+//Question #1;
+
+console.log(`This is Addition: ${5+3}`);  //8
+console.log(`This is Subtracion: ${10-2}`);  //8
+console.log(`This is Multiplication: ${4*2}`);  //8
+console.log(`This is division: ${16/2}`);  //8
+
+//Question #2;
+
+console.log(`This is Addition: ${4+4}`);  //8
+console.log(`This is Subtracion: ${20-12}`);  //8
+console.log(`This is Multiplication: ${2*4}`);  //8
+console.log(`This is division: ${24/3}`);   //8
+
+//Question #13;
+
+let fav_number: number = 7
+console.log(`My favourite number is :${fav_number}`);
