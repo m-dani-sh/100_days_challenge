1dfae00365c4339c14dfd56a4b6b06970e6c5ab0
Muhammad Danish <160328669+m-dani-sh@users.noreply.github.com>
2024-03-03 23:36:39 -0800

    day_3.js

---
+++ day_3.js
@@ -0,0 +1,19 @@
+//Question #1;
+
+console.log("This is Addition:".concat(5 + 3));
+console.log("This is Subtracion:".concat(10 - 2));
+console.log("This is Multiplication:".concat(4 * 2));
+console.log("This is division:".concat(16 / 2));
+
+//Question #2;
+
+console.log("This is Addition: ".concat(4 + 4)); //8
+console.log("This is Subtracion: ".concat(20 - 12)); //8
+console.log("This is Multiplication: ".concat(2 * 4)); //8
+console.log("This is division: ".concat(24 / 3)); //8
+
+//Question #3;
+
+var fav_number = 7;
+console.log("My favourite number is :".concat(fav_number));
+
