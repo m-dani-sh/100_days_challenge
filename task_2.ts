a3c1f05f00ed0b93972ef68d13d4b9ad5e0c6968
Muhammad Danish <160328669+m-dani-sh@users.noreply.github.com>
2024-03-03 23:29:30 -0800

    Delete task_2.ts

--- task_2.ts
+++
@@ -1,6 +0,0 @@
-//Question #2;
-
-console.log(`This is Addition: ${4+4}`);  //8
-console.log(`This is Subtracion: ${20-12}`);  //8
-console.log(`This is Multiplication: ${2*4}`);  //8
-console.log(`This is division: ${24/3}`);   //8
