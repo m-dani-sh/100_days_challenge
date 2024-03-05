b0092b60cf7f3f7bbc1838ae85a1b84c6500aace
Muhammad Danish <160328669+m-dani-sh@users.noreply.github.com>
2024-03-05 02:26:45 -0800

    day_4.ts

---
+++ day_4.ts
@@ -0,0 +1,26 @@
+//Question #1;
+
+//my name is danish 
+//let my_name: string = "Danish"
+//age is 19
+//let age: number = 19;
+
+
+//Question #2;
+
+let names: string[] = ["Danish", "Arsalan", "Ishaq"];
+
+names.forEach((name)=>{
+    //console.log(name);
+    
+});
+export default names;
+
+
+//Question #3;
+
+import names from "./task_2";
+names.forEach((name) => {
+    console.log(`Hello, ${name} Have a great day`)
+});
+
